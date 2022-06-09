d10719cf7db9e4c1b9d8d1278deb5043cd7493d7
analyceferreira <anallyce_@live.com>
2022-06-09 01:00:44 -0400

    Merge changes conficts in app.js

--- app.js
+++ app.js
@@ -7,11 +7,8 @@ const app = express()
 //-------------- IMPORT ROUTES -----------------
 const routersIndex = require(path.join(__dirname,'/Routers/index.js'))
 const routersProducts = require(path.join(__dirname,'/Routers/products.js'))
-<<<<<<< HEAD
 const routersUser = require(path.join(__dirname,'/Routers/user.js'))
-=======
 const routerCart = require(path.join(__dirname, '/Routers/cartRoutes.js'))
->>>>>>> 1d884eecc00ff9998c197654769adc874b383a7d
 
 
 
@@ -29,11 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 //-------------- ROUTES -----------------
 app.use('/', routersIndex)
 app.use('/loja', routersProducts)
-<<<<<<< HEAD
 app.use('/user', routersUser)
-=======
 app.use('/cart', routerCart),
->>>>>>> 1d884eecc00ff9998c197654769adc874b383a7d
 
 
 
