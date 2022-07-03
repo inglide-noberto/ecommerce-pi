cf629b82d4b4fea18d69ff76bcd6449ded3a4cb5
Inglide Noberto <inglidenoberto961@gmail.com>
2022-07-03 14:30:30 -0300

    atualizando EJS cart

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,8 +1,14 @@
 const dataorders = require('../data/data-orders.json')
+const productsData = require('../data/data-products.json')
+const dataCart = require('../data/data-cart.json')
+
+const fs = require('fs')
 
 const CartController = {
     index: (req,res)=>{
-        return res.render('layout', {'page': 'cart'})
+        let cart = dataCart[0]
+        
+        return res.render('layout', {'page': 'cart' , cart })
     },
     delivery: (req,res)=>{
         return res.render('layout', {'page': 'cart-delivery'})
@@ -12,6 +18,25 @@ const CartController = {
     },
     pedidoFinalizado:(req,res)=>{
         return res.send("sucesso")
+    },
+    //teste de vinculo products
+    show: (req,res) => {
+        const {slug} = req.params
+
+        const product = productsData.find(product => product.slug === slug)
+
+        if(product){
+            // retornar os dados
+
+            dataCart[0].products.push({ "products-id": product.id , "quantity": 1})
+            let carrinho = JSON.stringify(dataCart)
+            fs.writeFileSync( '/data/data-cart.json' , carrinho)
+
+            res.redirect('/')
+        }
+        else{
+            res.status(404).send()
+        }
     }
 }
 
