e9da3a201fc9910ce027fab3bcc312106619043d
Inglide Noberto <inglidenoberto961@gmail.com>
2022-08-28 21:56:37 -0300

    compartilhando codigo

--- public/scripts/quantity.js
+++ public/scripts/quantity.js
@@ -1,6 +1,12 @@
 var count = 0;
+var res =0;
 var btns = document.querySelectorAll('.button-quantity')
 var ipt = document.getElementById('soma');
+var preco = document.querySelector('.teste-price')
+var elm = document.querySelectorAll('.element')
+
+var valor = parseFloat(document.querySelector('.teste-price').value)
+
 
 btns.forEach(function(btn){
     btn.addEventListener('click', function(e){
@@ -9,16 +15,26 @@ btns.forEach(function(btn){
 
         if(option.contains('more')){
             count++
+            res = count * valor
+            document.querySelector('.teste-price').value = res;
         }
         else if(option.contains('less')){
             count--
-            if(count == 0)
-            
-            alert('CARRINHO ZERADO')
+            res = count * valor
+            document.querySelector('.teste-price').value = res;
+
+            if(count == 0){
+                 alert('CARRINHO ZERADO')
+                 document.querySelector('.teste-price').remove()
+            }
         }
         ipt.value = count
     })
 })
+
+module.exports = count
+
+
 
 
 
